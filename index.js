d2b6600befdaec2f5ac821efa3c3b68238a4b52a
abunaim34 <farhanadnanfarabi@gmail.com>
2024-06-06 16:59:22 +0600

    approve or reject session

--- index.js
+++ index.js
@@ -244,6 +244,20 @@ async function run() {
       res.send(result)
     })
 
+    app.put('/session/:id', async(req, res) => {
+      const id = req.params.id
+      const filter = {_id: new ObjectId(id)}
+      const updatestatus = req.body
+      const updatedDoc = {
+        $set: {
+          status: updatestatus.status,
+          registration_fee: updatestatus.registration_fee
+        }
+      }
+      const result = await sessionCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
